5b923e0aaa3140ae48697276c608d4fce73b530a
vieira <vieira.jas.abi@gmail.com>
2024-10-24 23:53:58 -0400

    nice project

--- Sections.js
+++ Sections.js
@@ -1,22 +1,83 @@
-class Outlet {
-    Outlets
-}
-
-class Inlet {
-    Inlets 
-}
-
-class WorkingSection {
-    WorkingSections
-}
-
-
-class Section {
-    constructor(name, picture, info) {
+class ValveSection {
+    constructor(name, info) {
         this._name = name;
-        this._picture = picture;
         this._info = info;
     }
 }
 
-export { Outlet, Inlet, WorkingSection, Section }
+export const inlets = [];
+inlets.push({ name: "Standard end inlet section", info: "Stuff about Standard inlets" });
+inlets.push({ name: "Pump unloading end inlet", info: "Stuff about Pump inlets" });
+inlets.push({ name: "Regenerative End Inlet", info: "Stuff about Regenerative inlets" });
+
+export const centers = [];
+centers.push({ name: "A20 & A35 Double Acting Cylinder Series", info: "Stuff about A20 middles" });
+centers.push({ name: "A20 & A35 Double Acting Motor Control", info: "Stuff about A20 middles" });
+centers.push({ name: "D-Line Double Acting Cylinder Control", info: "Stuff about D-Line middles" });
+centers.push({ name: "D-Line Double Acting Motor Control", info: "Stuff about D-Line middles" });
+centers.push({ name: "Double Acting Cylinder Control Parallel Series", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Control Tandem Only", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Control With Internal Make-Up", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Control", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Float Section Parallel Only", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Float Section Tandem Only", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Float Section", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Cylinder Parallel Regenerative", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Motor Control Tandem Only", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Motor Control With Internal Make-Up", info: "Stuff about middles" });
+centers.push({ name: "Double Acting Motor Control", info: "Stuff about middles" });
+centers.push({ name: "Midsection Flow Control Pressure Compensated", info: "Stuff about middles" });
+centers.push({ name: "Midsection Inlet combined flow", info: "Stuff about middles" });
+centers.push({ name: "Midsection Inlet split flow with pressure beyond port", info: "Stuff about middles" });
+centers.push({ name: "Midsection Inlet split flow", info: "Stuff about middles" });
+centers.push({ name: "Midsection Selector Inlet", info: "Stuff about middles" });
+centers.push({ name: "Single Acting Cylinder Control", info: "Stuff about middles" });
+centers.push({ name: "Regenerative Midsection Inlet", info: "Stuff about middles" });
+centers.push({ name: "Pump Unloading Midsection Inlet", info: "Stuff about middles" });
+
+export const outlets = [];
+outlets.push({ name: "Standard Tank Return Outlet Section", info: "Stuff about Standard outlets" });
+outlets.push({ name: "Pressure Beyond Outlet Section", info: "Stuff about Pressure outlets" });
+outlets.push({ name: "Pressure Compensator Outlet Section", info: "Stuff about Pressure outlets" });
+outlets.push({ name: "Regenerative Outlet Section", info: "Stuff about Regenerative outlets" });
+outlets.push({ name: "Flow Divider Outlet Section", info: "Stuff about Flow Divider outlets" });
+
+const inletNames = [
+    "Standard end inlet section",
+    "Pump unloading end inlet",
+    "Regenerative End Inlet",
+  ];
+
+  const outletNames = [
+    "Standard Tank Return Outlet Section",
+    "Pressure Beyond Outlet Section",
+    "Pressure Compensator Outlet Section",
+    "Regenerative Outlet Section",
+    "Flow Divider Outlet Section",
+  ];
+ 
+  const centerNames = [
+    "A20 & A35 Double Acting Cylinder Series",
+    "A20 & A35 Double Acting Motor Control",
+    "D-Line Double Acting Cylinder Control",
+    "D-Line Double Acting Motor Control",
+    "Double Acting Cylinder Control Parallel Series",
+    "Double Acting Cylinder Control Tandem Only",
+    "Double Acting Cylinder Control With Internal Make-Up",
+    "Double Acting Cylinder Control",
+    "Double Acting Cylinder Float Section Parallel Only",
+    "Double Acting Cylinder Float Section Tandem Only",
+    "Double Acting Cylinder Float Section",
+    "Double Acting Cylinder Parallel Regenerative",
+    "Double Acting Motor Control Tandem Only",
+    "Double Acting Motor Control With Internal Make-Up",
+    "Double Acting Motor Control",
+    "Midsection Flow Control Pressure Compensated",
+    "Midsection Inlet combined flow",
+    "Midsection Inlet split flow with pressure beyond port",
+    "Midsection Inlet split flow",
+    "Midsection Selector Inlet",
+    "Single Acting Cylinder Control",
+    "Regenerative Midsection Inlet",
+    "Pump Unloading Midsection Inlet",
+  ];
--- flipbookulator.js
+++ flipbookulator.js
@@ -1,101 +1,27 @@
+import { centers, inlets, outlets } from "./Sections.js";
+
 const maxNumPanes = 8;
-const minNumPanes = 1;
+const minNumPanes = 2;
 const valveSections = document.getElementById("valveSections");
 
 var decreaseButton = document.getElementById("decrease");
 decreaseButton.addEventListener("click", () => {
   if (panes.innerText > minNumPanes) {
     panes.innerText--;
-    if (panes.innerText > 2){        
-    createCenter(panes.innerText -2 );
-    }
+    removeCenter(panes.innerText - 1);
   }
 });
 
 var increaseButton = document.getElementById("increase");
 increaseButton.addEventListener("click", () => {
   if (panes.innerText < maxNumPanes) panes.innerText++;
-  if (panes.innerText > 2){        
-    createCenter(panes.innerText -2 );
-    }
+  addCenter(panes.innerText);
 });
-
-
-const inletNames = [
-  "Standard end inlet section",
-  "Pump unloading end inlet",
-  "Regenerative End Inlet",
-];
-const centerNames = [
-  "A20 & A35 Double Acting Cylinder Series",
-  "A20 & A35 Double Acting Motor Control",
-  "D-Line Double Acting Cylinder Control",
-  "D-Line Double Acting Motor Control",
-  "Double Acting Cylinder Control Parallel Series",
-  "Double Acting Cylinder Control Tandem Only",
-  "Double Acting Cylinder Control With Internal Make-Up",
-  "Double Acting Cylinder Control",
-  "Double Acting Cylinder Float Section Parallel Only",
-  "Double Acting Cylinder Float Section Tandem Only",
-  "Double Acting Cylinder Float Section",
-  "Double Acting Cylinder Parallel Regenerative",
-  "Double Acting Motor Control Tandem Only",
-  "Double Acting Motor Control With Internal Make-Up",
-  "Double Acting Motor Control",
-  "Midsection Flow Control Pressure Compensated",
-  "Midsection Inlet combined flow",
-  "Midsection Inlet split flow with pressure beyond port",
-  "Midsection Inlet split flow",
-  "Midsection Selector Inlet",
-  "Single Acting Cylinder Control",
-  "Regenerative Midsection Inlet",
-  "Pump Unloading Midsection Inlet",
-];
-const outletNames = [
-  "Standard Tank Return Outlet Section",
-  "Pressure Beyond Outlet Section",
-  "Pressure Compensator Outlet Section",
-  "Regenerative Outlet Section",
-  "Flow Divider Outlet Section",
-];
-
-const populateOutlet = function () {
-    var outletSection = document.createElement("section");
-    outletSection.id = "outlet";
-    outletSection.className = "valveSection"
-    let outletSelect = document.createElement("select");
-    outletSelect.id = "outletSelect";
-    outletSelect.className = "dropdown";
-    let outletLabel = document.createElement("label");
-    outletLabel.for = "outletSelect";
-    outletLabel.innerText = "Outlet:";
-    let outletImg = document.createElement("img");
-    outletImg.src = "images/outlet/Standard Tank Return Outlet Section.jpg"
-    outletImg.id = "outletImg"
-    outletImg.className = "valveSchematic"
-  
-    for (var x = 0; x < outletNames.length; x++) {
-      let outletOption = document.createElement("option");
-      outletOption.innerText = outletNames[x];
-      outletOption.id = "outletOption" + (x + 1);
-  
-      outletSelect.appendChild(outletOption);
-    }
-    outletSection.appendChild(outletLabel);
-    outletSection.appendChild(outletSelect);
-    outletSection.appendChild(outletImg);
-    valveSections.appendChild(outletSection);
-  
-    outletSelect.addEventListener("change", () => {
-      let outletImg = document.getElementById("outletImg");
-      outletImg.src = "images/outlet/" + outletSelect.value + ".jpg";
-    });
-  };
 
 const populateInlet = function () {
   let inletSection = document.createElement("section");
   inletSection.id = "inlet";
-  inletSection.className = "valveSection"
+  inletSection.className = "valveSection";
   let inletSelect = document.createElement("select");
   inletSelect.id = "inletSelect";
   inletSelect.className = "dropdown";
@@ -103,13 +29,16 @@ const populateInlet = function () {
   inletLabel.for = "inletSelect";
   inletLabel.innerText = "Inlet:";
   let inletImg = document.createElement("img");
-  inletImg.src = "images/inlet/Standard end inlet section.jpg"
-  inletImg.id = "inletImg"
-  inletImg.className = "valveSchematic"
+  inletImg.src = "images/inlet/Standard end inlet section.jpg";
+  inletImg.id = "inletImg";
+  inletImg.className = "valveSchematic";
+  let inletInfo = document.createElement("p");
+  inletInfo.id = "inletInfo";
+  inletInfo.innerHTML = inlets[0].info;
 
-  for (var x = 0; x < inletNames.length; x++) {
+  for (var x = 0; x < inlets.length; x++) {
     let inletOption = document.createElement("option");
-    inletOption.innerText = inletNames[x];
+    inletOption.innerText = inlets[x].name;
     inletOption.id = "inletOption" + (x + 1);
 
     inletSelect.appendChild(inletOption);
@@ -117,11 +46,53 @@ const populateInlet = function () {
   inletSection.appendChild(inletLabel);
   inletSection.appendChild(inletSelect);
   inletSection.appendChild(inletImg);
+  inletSection.appendChild(inletInfo);
   valveSections.appendChild(inletSection);
 
   inletSelect.addEventListener("change", () => {
-    let inletImg = document.getElementById("inletImg");
     inletImg.src = "images/inlet/" + inletSelect.value + ".jpg";
+    inletInfo.innerHTML = inlets.find(
+      (x) => x.name == inletSelect.value
+    ).info;
+  });
+};
+
+const populateOutlet = function () {
+  var outletSection = document.createElement("section");
+  outletSection.id = "outlet";
+  outletSection.className = "valveSection";
+  let outletSelect = document.createElement("select");
+  outletSelect.id = "outletSelect";
+  outletSelect.className = "dropdown";
+  let outletLabel = document.createElement("label");
+  outletLabel.for = "outletSelect";
+  outletLabel.innerText = "Outlet:";
+  let outletImg = document.createElement("img");
+  outletImg.src = "images/outlet/Standard Tank Return Outlet Section.jpg";
+  outletImg.id = "outletImg";
+  outletImg.className = "valveSchematic";
+  let outletInfo = document.createElement("p");
+  outletInfo.id = "outletInfo";
+  outletInfo.innerHTML = outlets[0].info;
+
+  for (var x = 0; x < outlets.length; x++) {
+    let outletOption = document.createElement("option");
+    outletOption.innerText = outlets[x].name;
+    outletOption.id = "outletOption" + (x + 1);
+
+    outletSelect.appendChild(outletOption);
+  }
+  outletSection.appendChild(outletLabel);
+  outletSection.appendChild(outletSelect);
+  outletSection.appendChild(outletImg);
+  outletSection.appendChild(outletInfo);
+  valveSections.appendChild(outletSection);
+
+  outletSelect.addEventListener("change", () => {
+    outletImg.src = "images/outlet/" + outletSelect.value + ".jpg";
+    outletInfo.innerHTML = outlets.find(
+      (x) => x.name == outletSelect.value
+    ).info;
   });
 };
 
@@ -135,11 +106,64 @@ const populateValues = function () {
     let inletImg = document.getElementById("inletImg");
     inletImg.src = "images/inlet/" + inletSelect.value + ".jpg";
   });
-  
+
   populateOutlet();
 };
 
 populateValues();
+
+const removeCenter = function (centerSections) {
+  let parent = document.getElementById("valveSections");
+
+  document.getElementById("center" + centerSections).remove();
+  //centerToRemove.parentNode.removeChild(centerToRemove);
+};
+
+const addCenter = function (centerSections) {
+  let centerSection = document.createElement("section");
+  centerSection.id = "center" + (centerSections - 2);
+  centerSection.className = "valveSection";
+  let centerSelect = document.createElement("select");
+  centerSelect.id = "centerSelect" + (centerSections - 2);
+  centerSelect.className = "dropdown";
+  let centerLabel = document.createElement("label");
+  centerLabel.for = "centerSelect" + (centerSections - 2);
+  centerLabel.innerText = "Center Valve " + (centerSections - 2) + ":";
+  let centerImg = document.createElement("img");
+  centerImg.src = "images/" + centers[0].name + ".jpg";
+  centerImg.id = "inletImg";
+  centerImg.className = "valveSchematic";
+  let centerInfo = document.createElement("p");
+  centerInfo.id = "info" + (centerSections - 2);
+  centerInfo.innerHTML = centers[0].info;
+
+  for (var x = 0; x < centers.length; x++) {
+    let centerOption = document.createElement("option");
+    centerOption.innerText = centers[x].name;
+    centerOption.id = "centerOption" + (x - 2);
+
+    centerSelect.appendChild(centerOption);
+  }
+  centerSection.appendChild(centerLabel);
+  centerSection.appendChild(centerSelect);
+  centerSection.appendChild(centerImg);
+  centerSection.appendChild(centerInfo);
+
+  centerSelect.addEventListener("change", () => {
+    centerImg.src = "images/" + centerSelect.value + ".jpg";
+    centerInfo.innerHTML = centers.find(
+      (x) => x.name == centerSelect.value
+    ).info;
+  });
+  let previous = document.getElementById("center" + (centerSections - 3));
+
+  if (previous) {
+    previous.after(centerSection);
+  } else {
+    previous = document.getElementById("inlet");
+    previous.after(centerSection);
+  }
+};
 
 const createCenter = function (elements) {
   let parent = document.getElementById("valveSections");
@@ -150,44 +174,13 @@ const createCenter = function (elements) {
 
   populateInlet();
 
-  for (var x = 0; x < elements; x++) {
-    createIndividualCenter(x);
-  }
+  for (var x = 0; x < elements; x++) {}
 
   populateOutlet();
 };
 
-const createIndividualCenter = function (x) {
-  let centerSection = document.createElement("section");
-  centerSection.id = "center" + (x + 1);
-  centerSection.className = "valveSection";
-  let centerSelect = document.createElement("select");
-  centerSelect.id = "centerSelect" + (x + 1);
-  centerSelect.className = "dropdown";  
-  let centerLabel = document.createElement("label");
-  centerLabel.for = "centerSelect" + (x + 1);
-  centerLabel.innerText = "Center Valve " + ( x + 1 ) + ":";
-  let centerImg = document.createElement("img");
-  centerImg.src = "images/inlet/Standard end inlet section.jpg"
-  centerImg.id = "inletImg"
-  centerImg.className = "valveSchematic"
-
-
-  for (var x = 0; x < centerNames.length; x++) {
-    let centerOption = document.createElement("option");
-    centerOption.innerText = centerNames[x];
-    centerOption.id = "centerOption" + (x + 1);
-
-    centerSelect.appendChild(centerOption);
-  }
-  centerSection.appendChild(centerLabel);
-  centerSection.appendChild(centerSelect);
-  centerSection.appendChild(centerImg);
-  valveSections.appendChild(centerSection);
-
-  centerSelect.addEventListener("change", () => {
-    centerImg.src = "images/" + centerSelect.value + ".jpg";
-  });
-};
+function createIndividualCenter(x) {
+  return centerSection;
+}
 
 // TODO: Rename pictures = SectionNames - " "
